86bbc16e3debc88122da26b19a5b6e74acf48062
Matthew J Doyle <matthew@tuttholland.com>
2024-04-18 13:52:05 +1000

    minor error fixes

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 class MyAop extends StatefulWidget {
   const MyAop({Key? key}) : super(key: key);
 
-
- @override
- _MyAopState createState() => _MyAopState();
+  @override
+  _MyAopState createState() => _MyAopState();
 }
-
 
 class _MyAopState extends State<MyAop> {
   @override
@@ -15,8 +13,8 @@ class _MyAopState extends State<MyAop> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          prefixIcon: Icon (Icons.arrow_circle_left_sharp)
-          title: Text('Pocket Money Manager')
+          prefixIcon: Icon(Icons.arrow_circle_left_sharp),
+          title: Text('Pocket Money Manager'),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -25,103 +23,98 @@ class _MyAopState extends State<MyAop> {
             Text(
               'login',
               style: TextStyle(
-                fontSize: 35,
-                color: Colors.black,
-                fontWeight: FontWeight.bold
-              ),
+                  fontSize: 35,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 30,
+            ),
             Form(
               child: Column(
                 children: [
-
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:15),
-                    child:TextFormField(
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: InputDecoration(
-                      labelText: 'Username',
-                      hintText: 'Enter username',
-                      prefixIcon: Icon(Icons.account_circle),
-                      border: OutlineInputBorder(),
-
-                    
-                    ),
-                    onChanged: (String value) {
-                      
-                    },
-                    validator: (value) {
-                      return value!.isEmpty? 'Please enter username' : null;
-                    },
-                  ),
-                  ),
-                  SizedBox(height: 30,),
-
-
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:15),
-                    child:TextFormField(
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      hintText: 'Enter password',
-                      prefixIcon: Icon(Icons.password),
-                      border: OutlineInputBorder(),
-
-                    ),
-                  
-                  
-                    onChanged: (String value) {
-                      
-                    },
-                    validator: (value) {
-                      return value!.isEmpty? 'Please enter password' : null;
-                    },
-                  ),
-                  ),
-                  SizedBox(height: 30,),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:35),
-                    child:  MaterialButton(
-                    minWidth: double.infinity,
-                    onPressed: () {},
-                    child: Text('Login'),
-                    color: Colors.grey[300],
-                    textColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        hintText: 'Enter username',
+                        prefixIcon: Icon(Icons.account_circle),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {},
+                      validator: (value) {
+                        return value!.isEmpty ? 'Please enter username' : null;
+                      },
                     ),
                   ),
-                  SizedBox(height: 100,),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter password',
+                        prefixIcon: Icon(Icons.password),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {},
+                      validator: (value) {
+                        return value!.isEmpty ? 'Please enter password' : null;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 35),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      onPressed: () {},
+                      child: Text('Login'),
+                      color: Colors.grey[300],
+                      textColor: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 100,
+                  ),
                   Text(
-              "don't have an acount",
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.grey[900],
-                
-              ),
-            ),
-                  SizedBox(height: 100,),
-                  
+                    "don't have an acount",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[900],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 100,
+                  ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:100),
-                    child:  MaterialButton(
-                    minWidth: double.infinity,
-                    onPressed: () {},
-                    child: Text('sign up',
-                    style: TextStyle(fontSize: 15,),
+                    padding: const EdgeInsets.symmetric(horizontal: 100),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      onPressed: () {},
+                      child: Text(
+                        'sign up',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                      color: Colors.grey[300],
+                      textColor: Colors.black,
                     ),
-                    color: Colors.grey[300],
-                    textColor: Colors.black,
-                    
-                    
-                    ),
-                  ),    
+                  ),
                 ],
               ),
             ),
-
           ],
         ),
       ),
-     );
+    );
   }
 }
